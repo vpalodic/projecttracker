a211e82a56132a40a4723a915234f039cf7bd2a7
Vincent J. Palodichuk <vj.palodichuk@gmail.com>
2014-01-01 10:20:28 -0600

    Added first migration

---
+++ protected/migrations/m140101_160135_create_project_table.php
@@ -0,0 +1,34 @@
+<?php
+
+class m140101_160135_create_project_table extends CDbMigration
+{
+	public function up()
+	{
+		$this->createTable('project',
+						   array('id' => 'pk',
+						    	 'name' => 'string NOT NULL',
+						   		 'description' => 'text NOT NULL',
+						   		 'create_time' => 'datetime DEFAULT NULL',
+						   		 'create_user_id' => 'int(11) DEFAULT NULL',
+						   		 'update_time' => 'datetime DEFAULT NULL',
+						   		 'update_user_id' => 'int(11) DEFAULT NULL'),
+						   		 'ENGINE = InnoDB DEFAULT CHARSET = utf8 AUTO_INCREMENT = 3')
+	}
+
+	public function down()
+	{
+		$this->dropTable('project');
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
