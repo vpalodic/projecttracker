18ffefaae89f10ef93806593e0ba40b190224045
Vincent J. Palodichuk <vj.palodichuk@gmail.com>
2014-01-01 00:37:42 -0600

    Project Tracker Yii Web App with YiiStrap, YiiMailer, and YiiWheels extensions

---
+++ protected/config/main.php
@@ -0,0 +1,111 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'Project Tracker',
+
+	'theme' => 'bootstrap',
+
+    'aliases' => array(
+        'bootstrap' => realpath(__DIR__ . '/../extensions/bootstrap'), // change this if necessary
+
+        'yiiwheels' => realpath(__DIR__ . '/../extensions/yiiwheels'), // change if necessary
+    ),
+
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// autoloading model and component classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+        'ext.YiiMailer.YiiMailer',
+        'bootstrap.helpers.TbHtml',
+	),
+
+	'modules'=>array(
+		'gii'=>array(
+            'generatorPaths' => array(
+                'bootstrap.gii',
+            ),
+			'class'=>'system.gii.GiiModule',
+			'password'=>'ProjectTracker2014#',
+			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'=>array('127.0.0.1','::1'),
+		),
+	),
+
+	// application components
+	'components'=>array(
+        // yiistrap configuration
+        'bootstrap' => array(
+            'class' => 'bootstrap.components.TbApi',
+        ),
+
+        // yiiwheels configuration
+        'yiiwheels' => array(
+            'class' => 'yiiwheels.YiiWheels',
+        ),
+
+		'user'=>array(
+			// enable cookie-based authentication
+			'allowAutoLogin'=>true,
+		),
+		// uncomment the following to enable URLs in path-format
+		/*
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			'rules'=>array(
+				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+			),
+		),
+		*/
+		/*
+		// uncomment the following to use a SQL Lite database
+		'db'=>array(
+			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+		),
+		*/
+		'db'=>array(
+			'connectionString' => 'mysql:host=127.0.0.1;dbname=projecttracker',
+			'emulatePrepare' => true,
+			'username' => 'project_tracker',
+			'password' => 'ProjectTracker2014#',
+			'charset' => 'utf8',
+		),
+		
+		'errorHandler'=>array(
+			// use 'site/error' action to display errors
+			'errorAction'=>'site/error',
+		),
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+				// uncomment the following to show log messages on web pages
+				/*
+				array(
+					'class'=>'CWebLogRoute',
+				),
+				*/
+			),
+		),
+	),
+
+	// application-level parameters that can be accessed
+	// using Yii::app()->params['paramName']
+	'params'=>array(
+		// this is used in contact page
+		'adminEmail'=>'webmaster@example.com',
+	),
+);
